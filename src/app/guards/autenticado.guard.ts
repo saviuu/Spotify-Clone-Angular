e78da43de8887c65478bb729f8896a1b69453b60
Sávio Bertoldi <77998135+saviuu@users.noreply.github.com>
2022-06-01 20:25:10 -0300

    Ajustes no método canLoad para guard de rotas

--- src/app/guards/autenticado.guard.ts
+++ src/app/guards/autenticado.guard.ts
@@ -22,15 +22,13 @@ export class AutenticadoGuard implements CanLoad {
         return this.naoAutenticado();
       }
 
-    return new Promise((res) => {
-      const usuarioCriado = this.spotifyService.inicializarUsuario();
+    return new Promise(async (res) => {
+      const usuarioCriado = await this.spotifyService.inicializarUsuario();
       if(usuarioCriado)
         res(true);
       else
         res(this.naoAutenticado());
     })
-
-    return true;
   }
 
   naoAutenticado(){
